8302d24b7e9eabf9c3efbea383a1d432b20f1bc2
kevinwangkk <showmeeh@126.com>
2017-06-15 17:29:23 +0800

    添加模板
    .c .h 模板

---
+++ 模板.c
@@ -0,0 +1,63 @@
+/**
+  ******************************************************************************
+  * @file    SIM5215E.c
+  * @author  
+  * @version V1.0
+  * @date    20-May-2017
+  * @brief   This file provides all the SIM5215E functions.
+  ******************************************************************************
+  * @attention
+  *
+  * SIM5215E
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+
+
+/**
+  * @brief  串口接收命令解析
+  * @param  AT命令
+  * @param  AT参数
+  * @retval None
+  */
+
+
+/*////////////hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh*/
+
+
+/**
+  ******************************************************************************
+  * @file    SIM5215E.h
+  * @author  
+  * @version V1.0
+  * @date    20-May-2017
+  * @brief   This file contains all the functions prototypes for the sim5215E
+  ******************************************************************************
+  * @attention
+  *
+  * SIM5215E
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __SIM5215E_H_
+#define __SIM5215E_H_
+
+/* Includes ------------------------------------------------------------------*/
+
+/** 
+  * @brief  GSM command state enum definition  
+  */
+
+/** 
+  * @brief  SMS content structure definition  
+  */
+
+/**
+  * @brief GSM variable Functions
+  */
+
+
+#endif //SIM5215E.h
+
